df6db1733f21c5538134697c430bead312bc967b
your name here <abirmahamud610@gmail.com>
2021-08-04 14:46:57 -0400

    Add show page details

--- frontend/component/events/event_detail.jsx
+++ frontend/component/events/event_detail.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 class EventDetail extends React.Component {
     constructor(props){
--- frontend/component/events/event_show.jsx
+++ frontend/component/events/event_show.jsx
@@ -1,1 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import EventDetail from './event_detail';
+import { ProtectedRoute } from '../../utils/route_utils';
+
+
+const EventShow = ({ event, eventId, fetchEvent }) => {
+    const events = {
+        [eventId]: event 
+    };
+
+    return(
+        <div className="single-event-show">
+            <div className="back-to-events">
+                <Link to="/events">Back to Events</Link>
+            </div>
+            <div className="event-details">
+                <EventDetail event={event}/>
+            </div>
+        </div>
+    )
+}
