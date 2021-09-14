9d0801f61985f04fc841ada457aad6b01837ff61
your name here <abirmahamud610@gmail.com>
2021-09-14 19:39:57 -0400

    Fix show page issue

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api, defaults: {format: :json} do
-    resources :users, only: [:create]
-    resources :events, only: [:create, :update, :destroy, :show, :index ]
-    resources :users do 
-        resources :events, only: [:index]
+    resources :users, only: [:create] do
+      resources :registrations, only: [:index]
     end
-    resource :session, only: [:create, :destroy, :show]
+    resource :session, only: [:create, :destroy]
+    resources :events, only: [:index, :show, :create, :update, :destroy] do
+      resources :registrations, only: [:create, :destroy]
+    end
   end
 
 
